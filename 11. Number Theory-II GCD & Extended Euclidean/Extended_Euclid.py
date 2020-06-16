4450d7ced8f2b8e5be39a68d1852193daefeef69
Shashank Sinha <34626597+ShashankSinha98@users.noreply.github.com>
2020-06-16 15:36:32 +0530

    Number Theory-1 Completed

---
+++ 11. Number Theory-II GCD & Extended Euclidean/Extended_Euclid.py
@@ -0,0 +1,23 @@
+import sys
+x = sys.maxsize
+y = sys.maxsize
+GCD = sys.maxsize
+
+def ExtendedEuclid(a,b):
+    global x, y, GCD
+
+    if b==0:
+        x=1
+        y=0
+        GCD = a
+        return
+
+    ExtendedEuclid(b,a%b)
+    cX = y
+    cY = x - (a//b)*y
+
+    x = cX
+    y = cY
+
+ExtendedEuclid(18,30)
+print(x,y,GCD)
---
+++ 11. Number Theory-II GCD & Extended Euclidean/GCD.py
@@ -0,0 +1,5 @@
+# a x b = LCM(a, b) * GCD (a, b)
+def GCD(a,b):
+    return a if b==0 else GCD(b,a%b)
+      
+print(GCD(12,20))
