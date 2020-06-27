5b758fcc382936d35d4327398802d74ad9b46eaf
Shashank Sinha <34626597+ShashankSinha98@users.noreply.github.com>
2020-06-27 16:56:31 +0530

    Added Code

--- 13. Number Theory - IV Linear Diophantine Equations/The_Football_Season.py
+++ 13. Number Theory - IV Linear Diophantine Equations/The_Football_Season.py
@@ -1,47 +1,56 @@
+# Acepted on Codeforces
 # ax + by = c, I/P = a,b,c O/P = values of x and y
 import sys
-def gcd(a,b):
-    return a if b==0 else gcd(b,a%b)
+def GCD(a,b):
+    return a if b==0 else GCD(b,a%b)
 
-x = y = GCD = sys.maxsize
-def extended_euclid(a,b):
-    global x,y,GCD
+x = y = sys.maxsize
+def ExtendedEuclid(a,b):
+    global x,y
     if b==0:
         x = 1
         y = 0
-        GCD = a
         return
 
-    extended_euclid(b,a%b)
+    ExtendedEuclid(b,a%b)
     cX = y
     cY = x - (a//b)*y
 
     x = cX
     y = cY
 
+def invModulo(a,m):
+    if GCD(a,m) != 1:
+        raise Exception('No inverse modulo exist for given a and m')
+    else:
+        ExtendedEuclid(a,m)
+        return (x+m)%m
+
 
 
 n,p,w,d = [int(i) for i in input().split()]
 
-if p%gcd(w,d)!=0:
+if p%GCD(w,d)!=0:
     print("-1") 
-
+elif n*w < p:
+    print("-1") 
 else:
-    extended_euclid(w,d)
-    k = p//gcd(w,d)
-    print(x,y)
-    print(k*x,k*y)
-    if x<0 or y<0:
+    g = GCD(w,d)
+
+    p//=g
+    w//=g
+    d//=g
+
+    y =  ( (p%w) * invModulo(d,w) )%w
+    x = (p-y*d)//w
+
+    if x+y > n:
         print("-1")
-    elif k*x + k*y > n:
-        print("-1")
-    else:
-        z = n - (k*x+k*y)
-        print(k*x,k*y,z)
         
+    
+    elif x<0:
+        print("-1")
 
-
-
-
-
-
+    else:
+        z = n-(x+y)
+        print(x,y,z)
